93987111ec5ef99a13cfd6e14f096578e4b312d6
Tambarie <gt.gbaragbo@gmail.com>
2021-01-29 10:47:10 +0100

    warning from vscode

--- django-gbaragboscrumy/gbaragboscrumy/views.py
+++ django-gbaragboscrumy/gbaragboscrumy/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def get_grading_parameters(request):
-    return HttpResponse('Welcome to Django')
+    return HttpResponse('This is a Scrum Application')
