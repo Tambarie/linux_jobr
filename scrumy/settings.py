cc61fb1e584388fd1adf1d85da0819b66b4c7988
Tambarie <gt.gbaragbo@gmail.com>
2021-01-29 12:24:02 +0100

    working fine

--- scrumy/settings.py
+++ scrumy/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'gbaragboscrumy.apps.GbaragboscrumyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
