94e23640f89b66fff8dc0a7b7e7a5a286cc5f532
Tambarie <gt.gbaragbo@gmail.com>
2021-02-01 23:12:36 +0100

    Added models to the app. lab 8

--- gbaragboscrumy/admin.py
+++ gbaragboscrumy/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from . models import GoalStatus, ScrumyGoals,ScrumyHistory
+
+admin.site.register(GoalStatus)
+admin.site.register(ScrumyHistory)
+admin.site.register(ScrumyGoals)
+
 # Register your models here.
--- gbaragboscrumy/models.py
+++ gbaragboscrumy/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+
+
+class User(AbstractUser):
+    # username = models.CharField(max_length=20)
+    # first_name = models.CharField(max_length=20)
+    # last_name = models.CharField(max_length=20)
+    # password = models.CharField(max_length=20)
+    # email = models.EmailField(max_length=20)
+    pass
+
+
 
 # Create your models here.
+
+class GoalStatus(models.Model):
+    status_name = models.CharField(max_length=200)
+
+
+class ScrumyGoals (models.Model):
+   goal_name =models.CharField(max_length=20)
+   goal_id = models.IntegerField()
+   created_by = models.CharField(max_length=20)
+   moved_by =models.CharField(max_length=20)
+   owner = models.CharField(max_length=20)
+   user = models.ForeignKey(User, on_delete=models.CASCADE)
+   goal_status = models.ForeignKey(GoalStatus,on_delete=models.PROTECT)
+
+
+
+
+class ScrumyHistory(models.Model):
+    moved_by = models.CharField(max_length=20)
+    created_by = models.CharField(max_length=20)
+    moved_from = models.CharField(max_length=20)
+    moved_to = models.CharField(max_length=20)
+    time_of_action = models.DateTimeField(auto_now_add=True)
+    goal = models.ForeignKey(ScrumyGoals, on_delete=models.CASCADE)
+   
+
