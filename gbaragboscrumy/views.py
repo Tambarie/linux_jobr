55c28cc5ca99e6965c58e6f309eed6c5eff75908
Tambarie <gt.gbaragbo@gmail.com>
2021-02-03 01:02:34 +0100

    creating django views with arguments

--- gbaragboscrumy/views.py
+++ gbaragboscrumy/views.py
@@ -1,3 +1,4 @@
+from .models import ScrumyGoals
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -5,3 +6,8 @@ from django.http import HttpResponse
 # Create your views here.
 def get_grading_parameters(request):
     return HttpResponse('Learn Django')
+
+def move_goal(request, goal_id):
+    # movegoal = ScrumyGoals.objects.get(pk =1)
+    # print(movegoal)
+    return HttpResponse("Learn Django%s" % goal_id) 
--- scrumy/urls.py
+++ scrumy/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
+from gbaragboscrumy import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('gbaragboscrumy', include('gbaragboscrumy.urls'))
+    path('gbaragboscrumy', include('gbaragboscrumy.urls')),
+    path('movegoal/<int:goal_id>', views.move_goal)
 ]
